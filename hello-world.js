5d4c61f8e169e58cd96f4588c31a2114118c59ca
Saad Zia <saad.zia@enablersinc.com>
2019-08-09 16:53:45 +0500

    Second commit

--- hello-world.js
+++ hello-world.js
@@ -1,5 +1,3 @@
-// You can run the JS file with node {file_name}. You can ignore the '.js' extension when running the file.
-// For example to run this file write: node hello-world and you will get the results on console.
+
 
 console.log('Hello World!');
-console.log('Hello WorldFrom Saad');
